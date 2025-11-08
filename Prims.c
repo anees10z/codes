72ea03aeddc40efc842b9eb472756879b47ef4fe
Md Anees Khan <khanmdanis145@gmail.com>
2025-11-08 12:31:40 +0530

    Implement Prim's algorithm for minimum spanning tree

---
+++ Prims.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+void prims(int n, int cost[10][10]) {
+    int visited[10] = {0};
+    int i, j, u, v, ne = 1, minCost = 0, min;
+
+    visited[0] = 1; // start from first vertex
+
+    printf("\nEdges in MST:\n");
+    while (ne < n) {
+        min = 999;
+        for (i = 0; i < n; i++) {
+            if (visited[i] == 1) {
+                for (j = 0; j < n; j++) {
+                    if (visited[j] == 0 && cost[i][j] < min) {
+                        min = cost[i][j];
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+
+        printf("Edge %d: (%d -> %d) cost = %d\n", ne, u, v, min);
+        minCost += min;
+        visited[v] = 1;
+        ne++;
+    }
+    printf("\nMinimum cost = %d\n", minCost);
+}
+
+int main() {
+    int n = 3;
+    int cost[10][10] = {
+        {0, 5, 10},
+        {5, 0, 5},
+        {10, 5, 0}
+    };
+
+    // replace 0 with infinity
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (cost[i][j] == 0)
+                cost[i][j] = 999;
+        }
+    }
+
+    prims(n, cost);
+    return 0;
+}
+
+
+/*#include <stdio.h>
+#include <conio.h>
+
+void prims(int n, int cost[10][10]);
+
+int main()
+{
+    int i, j, n = 3, cost[10][10] = { {0, 5, 10},
+                                       {5, 0, 5},
+                                       {10, 5, 0} };
+
+    for (i = 0; i < n; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (cost[i][j] == 0) cost[i][j] = 999;
+        }
+    }
+    prims(n, cost);
+    getch();
+    return 0;
+}
+
+void prims(int n, int cost[10][10])
+{
+    int i, j, u, v, min, minCost = 0, visited[10] = {0};
+    visited[0] = 1;
+    for (i = 0; i < n; ++i)
+    {
+        min = 999;
+        for (j = 0; j < n; ++j)
+        {
+            if (cost[i][j] < min)
+            {
+                min = cost[i][j];
+                u = i;
+                v = j;
+            }
+        }
+        if (visited[v] == 0)
+        {
+            minCost = minCost + min;
+            visited[v] = 1;
+        }
+        cost[u][v] = cost[v][u] = 999;
+    }
+    printf("\nMin cost = %d", minCost);
+}*/
+
